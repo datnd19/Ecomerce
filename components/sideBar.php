10bc05e0efa80829ae3afd906b6c1c01b9351a62
Dac Dat <datnddev19@gmail.com>
2023-11-24 11:11:35 +0700

    first commit

---
+++ components/sideBar.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Sidebar 03</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+	
+		<nav id="sidebar" class="active">
+			<div class="custom-menu">
+				<button type="button" id="sidebarCollapse" class="btn btn-primary">
+					<i class="fa fa-bars"></i>
+					<span class="sr-only">Toggle Menu</span>
+				</button>
+			</div>
+			<div class="p-4">
+				<h1><a href="index.html" class="logo">Flash</a></h1>
+				<ul class="list-unstyled components mb-5">
+					<li class="active">
+						<a href="#"><span class="fa-solid fa-chart-line mr-3"></span> Dashboard</a>
+					</li>
+					<li>
+						<a href="listUser.php"><span class="fa fa-user mr-3"></span> User</a>
+					</li>
+					<li>
+						<a href="#"><span class="fa fa-briefcase mr-3"></span> Portfolio</a>
+					</li>
+					<li>
+						<a href="#"><span class="fa fa-sticky-note mr-3"></span> Blog</a>
+					</li>
+					<li>
+						<a href="#"><span class="fa fa-paper-plane mr-3"></span> Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+	
+
+	<script src="../js/jquery.min.js"></script>
+	<script src="../js/popper.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	<script src="../js/main.js"></script>
+</body>
+
+</html>
