d71f33b30287da17b43c35deb1b882c2321c1b53
unknown <datnddev19@gmail.com>
2023-12-17 00:03:34 +0700

    list cart

--- cart.php
+++ cart.php
@@ -8,6 +8,40 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="./css/style.css">
+    <style>
+        body {
+            background: #eee;
+        }
+
+        .ui-w-40 {
+            width: 40px !important;
+            height: auto;
+        }
+
+        .card {
+            box-shadow: 0 1px 15px 1px rgba(52, 40, 104, .08);
+        }
+
+        .ui-product-color {
+            display: inline-block;
+            overflow: hidden;
+            margin: .144em;
+            width: .875rem;
+            height: .875rem;
+            border-radius: 10rem;
+            -webkit-box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.15) inset;
+            box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.15) inset;
+            vertical-align: middle;
+        }
+
+        .dropdown-toggle::after {
+            content: none;
+        }
+
+        .dropdown-toggle {
+            background-color: transparent;
+        }
+    </style>
 </head>
 
 <body>
@@ -34,28 +68,8 @@
                                         <th class="text-center align-middle py-3 px-0" style="width: 40px;"><a href="#" class="shop-tooltip float-none text-light" title="" data-original-title="Clear cart"><i class="ino ion-md-trash"></i></a></th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                    <c:set var="check" value="0"></c:set>
-                                    <c:forEach var="c" items="${listcart}">
-                                        <tr>
-                                            <c:forEach var="pc" items="${listProdColor}">
-                                                <c:if test="${pc.product_color_id == c.product_color_id}">
-                                                    <c:forEach var="p" items="${listProd}">
-                                                        <c:if test="${pc.product_id == p.product_id}">
-                                                            <td class="p-4">${p.name}</td>
-                                                        </c:if>
-                                                    </c:forEach>
-                                                    <td class="p-4">${pc.color}</td>
-                                                    <td class="p-2"><img src="images/${pc.image}" alt="alt" style="width: 70px;height: 70px  "></td>
-                                                    <td class="text-right font-weight-semibold align-middle p-4 price${check}">${pc.price}</td>
-                                                    <td class="align-middle p-4"><input type="number" id="quantity" class="quantity" name="quantity" data-item="${c.product_color_id}" class="form-control text-center" min="1" max="${pc.quantity}" value="${c.quantity}"></td>
-                                                </c:if>
-                                            </c:forEach>
-                                            <td class="text-right font-weight-semibold align-middle p-4 total${check}" style="width: 150px">$115.1</td>
-                                            <td class="text-center align-middle px-0"><a onclick="doDelete(${c.product_color_id})" class="shop-tooltip close float-none text-danger removeBtn" title="" data-original-title="Remove">×</a></td>
-                                            <c:set var="check" value="${check+1}"></c:set>
-                                        </tr>
-                                    </c:forEach>
+                                <tbody class="cartTable">
+
                                 </tbody>
                             </table>
                         </div>
@@ -87,6 +101,82 @@
     <script src="./js/main.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        const viewCart = function() {
+            $.ajax({
+                url: 'http://localhost:3000/database/controller/cartController.php',
+                type: 'GET',
+                data: {
+                    action: 'viewCart',
+                },
+                success: (response) => {
+                    let data = JSON.parse(response);
+                    let html = "";
+                    data.forEach(function(item, currentIndex) {
+                        html += `<tr>
+                                    <td class="p-4">${item.dataProduct[0].product_name}</td>
+                                    <td class="p-4">${item.dataProductColor[0].color}</td>
+                                    <td class="p-2 d-flex flex-wrap">`;
+                        item.dataProductImage.forEach(function(item, currentIndex) {
+                            html += `<img src="images/${item.image}" alt="alt" style="width: 80px;height: 80px; margin-right:5px;object-fit:cover">`;
+                        })
+                        html += `</td>
+                                    <td class="text-right font-weight-semibold align-middle p-4 price${currentIndex}">${item.dataProductColor[0].price}</td>
+                                    <td class="align-middle p-4"><input type="number" id="quantity" class="quantity" name="quantity" data-item="${item.dataProductColor[0].product_color_id}" class="form-control text-center" min="1" max="${item.dataProductColor[0].quantity}" value="${item.quantity[0]}"></td>
+                                    <td class="text-right font-weight-semibold align-middle p-4 total${currentIndex}" style="width: 150px">$115.1</td>
+                                    <td class="text-center align-middle px-0"><a onclick="doDelete(${item.dataProductColor[0].product_color_id})" class="shop-tooltip close float-none text-danger removeBtn" title="" data-original-title="Remove">×</a></td>
+                                    </tr>`;
+                    })
+                    const cartTable = document.querySelector('.cartTable');
+                    cartTable.innerHTML = html;
+
+                    const quantity = document.querySelectorAll(".quantity");
+                    quantity.forEach((value, index) => {
+                        value.addEventListener("input", function(e) {
+                            if (+e.target.value > +value.max) { // Convert both value and max to numbers for comparison                                     
+                                e.target.value = value.max;
+                            }
+                            init();
+                        });
+                    });
+                    const init = function() {
+                        const totalAll = document.querySelector("strong");
+                        let totalCart = 0;
+                        quantity.forEach((value, index) => {
+                            const price = document.querySelector(`.price${index}`);
+                            const total = document.querySelector(`.total${index}`);
+                            total.innerHTML = '$' + Number(price.innerHTML) * Number(value.value);
+                            totalCart += Number(price.innerHTML) * Number(value.value);
+                        });
+                        totalAll.innerHTML = '$' + totalCart;
+                    };
+                    init();
+
+                    const quantityInputs = Array.from(document.querySelectorAll('.quantity'));
+                    quantityInputs.forEach((input) => {
+                        input.onchange = () => {
+                            $.ajax({
+                                type: "post",
+                                url: "http://localhost:3000/database/controller/cartController.php",
+                                data: {
+                                    productColorID: input.dataset.item,
+                                    quantity: input.value,
+                                    action: 'updateCartQuantity',   
+                                },
+                                cache: false,
+                                success: function(response) {
+                                    
+                                }
+                            });
+                        }
+                    });
+                }
+            })
+        }
+        viewCart();
+    </script>
+
 </body>
 
 </html>
--- database/controller/cartController.php
+++ database/controller/cartController.php
@@ -27,3 +27,45 @@ if (isset($_POST['action']) && $_POST['action'] == 'addToCart') {
         return;
     }
 }
+
+if (isset($_GET['action']) && $_GET['action'] == 'viewCart') {
+    session_start();
+    $jsonData = $_SESSION['account'];
+    $data = json_decode($jsonData, true);
+    $user_id = $data[0]['user_id'];
+    $sql = "SELECT * FROM cart WHERE user_id = '$user_id'; ";
+    $data = Query($sql, $connection);
+    $resultArray = [];
+    foreach ($data as $row) {
+        $productColor = $row['product_color_id']; // Assuming the column name is 'product_color'
+        $sqlProductColor = "SELECT * FROM product_color WHERE product_color_id = '$productColor'";
+        $dataProductColor = Query($sqlProductColor, $connection);
+
+        $productId = $dataProductColor[0]['product_id'];
+        $sqlProduct = "SELECT product_name FROM product WHERE product_id = '$productId'";
+        $dataProduct = Query($sqlProduct, $connection);
+
+        $sqlProductImages = "SELECT * FROM product_image WHERE product_color_id = '$productColor'";
+        $dataProductImage = Query($sqlProductImages, $connection);
+        $combinedData = [
+            "dataProductColor" => $dataProductColor,
+            "dataProduct" => $dataProduct,
+            "dataProductImage" => $dataProductImage,
+            "quantity" => $row['quantity'],
+        ];
+        $resultArray[] = $combinedData;
+    }
+    echo json_encode($resultArray);
+}
+
+if (isset($_POST['action']) && $_POST['action'] == 'updateCartQuantity') {
+    $productColorID = $_POST['productColorID'];
+    $quantity = $_POST['quantity'];
+    session_start();
+    $jsonData = $_SESSION['account'];
+    $data = json_decode($jsonData, true);
+    $user_id = $data[0]['user_id'];
+    $sqlUpdateCart = "UPDATE `cart` SET `quantity` = '$quantity' WHERE `user_id` = '$user_id' AND `product_color_id` = '$productColorID'";
+    $dataUpdateCart = Query($sqlUpdateCart, $connection);
+}
+
